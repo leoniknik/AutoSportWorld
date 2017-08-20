5def2ef91eca2c70adbbe9da74aca363e2fdf286
VolodinK <VolodinK@binbank.ru>
2017-08-20 20:28:18 +0300

    add images for tabbar

--- AutoSportWorld/AutoSportWorld/AppDelegate.swift
+++ AutoSportWorld/AutoSportWorld/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //настройка цвета фона navigationbar
         UINavigationBar.appearance().barTintColor = UIColor.ASWColor.black
+        
+        //настройка цвета таб бара
+        UITabBar.appearance().barTintColor = UIColor.ASWColor.black
+        //настройка цвета выделенного tab item
+        UITabBar.appearance().tintColor = UIColor.ASWColor.yellow
+        
         return true
     }
 
